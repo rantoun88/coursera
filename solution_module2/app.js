cc97f2f0ac091f91da36667fe3c20364e8b7c0f6
Roula Antoun <roula.antoun@saxsys.de>
2020-05-25 22:12:37 +0200

    Add the shopping list check off solution

---
+++ solution_module2/app.js
@@ -0,0 +1,83 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+// LIST 1
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuyCtrl = this;
+
+  
+  toBuyCtrl.toBuyList = ShoppingListCheckOffService.getToBuyItems();
+
+  
+  toBuyCtrl.buyItem = function(item) {
+    ShoppingListCheckOffService.buyItem(item);
+  }
+}
+
+
+// LIST 2
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var alreadyBoughtCtrl = this;
+  
+  alreadyBoughtCtrl.alreadyBoughtList = ShoppingListCheckOffService.getBoughtItems();
+  
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var toBuyItems = [
+    {
+      name : 'bonbons',
+      quantity: 7
+    },
+    {
+      name : 'chocolate',
+      quantity: 15
+    },
+    {
+      name : 'chips',
+      quantity: 10
+    },
+    {
+      name : 'cookies',
+      quantity: 10
+    }
+  ];
+  var boughtItems = [];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    boughtItems.push(item);
+  };
+
+  service.removeItem = function (itemIndex) {
+    toBuyItems.splice(itemIndex, 1);
+  };
+
+  service.buyItem = function(item) {
+    service.removeItem(item);
+    service.addItem(item.name , item.quantity);
+  }
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
